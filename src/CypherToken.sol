5df1321d29fb5adc3c2a32523b6e8da9be0440c2
Kurt Barry <kurt.m.barry@gmail.com>
2024-12-30 13:38:15 -0500

    remove forge init junk, create basic scaffolding

---
+++ src/CypherToken.sol
@@ -0,0 +1,7 @@
+pragma solidity =0.8.28;
+
+import "./interfaces/ICypherToken.sol";
+
+/// @title CypherToken
+/// @notice The token for the Cypher ecosystem.
+contract CypherToken is ICypherToken {}
---
+++ test/CypherToken.t.sol
@@ -0,0 +1,9 @@
+pragma solidity =0.8.28;
+
+import "forge-std/Test.sol";
+
+import "../src/CypherToken.sol";
+
+contract CyperTokenTest is Test {
+    function testSanity() public {}
+}
