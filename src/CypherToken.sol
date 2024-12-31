93cf31611eecc588e6547dbe09e9a7bc20cbc018
Kurt Barry <kurt.m.barry@gmail.com>
2024-12-30 21:38:32 -0500

    basic token impl

--- src/CypherToken.sol
+++ src/CypherToken.sol
@@ -1,7 +1,15 @@
 pragma solidity =0.8.28;
 
 import "./interfaces/ICypherToken.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /// @title CypherToken
 /// @notice The token for the Cypher ecosystem.
-contract CypherToken is ICypherToken {}
+contract CypherToken is ICypherToken, ERC20Permit {
+    constructor(address treasury) ERC20("Cypher", "CYPR") ERC20Permit("Cypher") {
+       // TODO: mint tokens to appropriate recipients or decide to do this via transfers
+       // from the treasury
+       _mint(treasury, 499_500_000);
+    }
+}
--- test/CypherToken.t.sol
+++ test/CypherToken.t.sol
@@ -5,5 +5,11 @@ import "forge-std/Test.sol";
 import "../src/CypherToken.sol";
 
 contract CyperTokenTest is Test {
-    function testSanity() public {}
+    function testConstruction() public {
+        CypherToken token = new CypherToken(address(this));
+
+        assertEq(token.name(), "Cypher");
+        assertEq(token.symbol(), "CYPR");
+        assertEq(token.totalSupply(), 499_500_000);
+    }
 }
