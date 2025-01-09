4856a0e4d83fca72630df6269c0b06885cd87a23
Kurt Barry <kurt.m.barry@gmail.com>
2025-01-08 21:08:24 -0500

    clock override, testing

--- src/CypherToken.sol
+++ src/CypherToken.sol
@@ -3,7 +3,8 @@ pragma solidity =0.8.28;
 import "./interfaces/ICypherToken.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit, Nonces} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {ERC20Votes, Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Time} from "@openzeppelin/contracts/utils/types/Time.sol";
 
 /// @title CypherToken
 /// @notice The token for the Cypher ecosystem.
@@ -20,5 +21,16 @@ contract CypherToken is ICypherToken, ERC20Permit, ERC20Votes {
 
     function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
         return ERC20Permit.nonces(owner);
+    }
+
+    function clock() public view override returns (uint48) {
+        return Time.timestamp();
+    }
+
+    function CLOCK_MODE() public view override returns (string memory) {
+        if (clock() != Time.timestamp()) {
+            revert ERC6372InconsistentClock();
+        }
+        return "mode=timestamp";
     }
 }
