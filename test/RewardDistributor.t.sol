98d2e185452ba91f4c823f8d6fcfa5acfc6be1ad
Kurt Barry <kurt.m.barry@gmail.com>
2025-01-14 01:26:50 -0500

    simple claim test

--- test/RewardDistributor.t.sol
+++ test/RewardDistributor.t.sol
@@ -55,4 +55,50 @@ contract RewardDistributorTest is Test {
         vm.expectRevert();
         rd.addRoot(root);
     }
+
+    function testClaimSingle() public {
+        (bytes32 root, address[] memory addrs, uint256[] memory amnts, bytes32[] memory proof) = _constructSimpleTree();
+        uint256 id = rd.addRoot(root);
+
+        cypher.transfer(address(rd), amnts[1]);
+        uint256 balBefore = cypher.balanceOf(addrs[1]);
+
+        vm.prank(addrs[1]);
+        rd.claim(proof, id, amnts[1]);
+
+        uint256 balAfter = cypher.balanceOf(addrs[1]);
+        assertEq(balAfter - balBefore, amnts[1]);
+        assertTrue(rd.claimed(id, addrs[1]));
+    }
+
+    function _constructSimpleTree() internal pure returns (bytes32 root, address[] memory addrs, uint256[] memory amnts, bytes32[] memory proof) {
+        addrs = new address[](4);
+        amnts = new uint256[](4);
+
+        addrs[0] = address(0x1234);
+        addrs[1] = address(0x6789);
+        addrs[2] = address(0x8888);
+        addrs[3] = address(0xF1F0);
+
+        amnts[0] = 5e18;
+        amnts[1] = 2.7e18;
+        amnts[2] = 555e18;
+        amnts[3] = 0.01e18;
+
+        proof = new bytes32[](2);
+
+        bytes32[] memory leafHashes = new bytes32[](4);
+        for(uint256 i = 0; i < 4; i++) {
+            leafHashes[i] = keccak256(bytes.concat(keccak256(abi.encode(addrs[i], amnts[i]))));
+        }
+
+        proof[0] = leafHashes[0];
+
+        bytes32 leftHash  = keccak256(bytes.concat(leafHashes[0], leafHashes[1]));
+        bytes32 rightHash = keccak256(bytes.concat(leafHashes[2], leafHashes[3]));
+
+        proof[1] = rightHash;
+
+        root = keccak256(bytes.concat(leftHash, rightHash));
+    }
 }
