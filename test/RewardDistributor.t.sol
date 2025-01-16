a1972aa30f0f7460117a66a634b919bdcefd3e30
Kurt Barry <kurt.m.barry@gmail.com>
2025-01-16 00:05:52 -0500

    tests for claimMultiple

--- test/RewardDistributor.t.sol
+++ test/RewardDistributor.t.sol
@@ -126,6 +126,113 @@ contract RewardDistributorTest is Test {
         rd.claim(proof, id, amnt);
     }
 
+    function testClaimMultiple() public {
+        address[] memory addrs = new address[](4);
+        uint256[] memory amnts = new uint256[](4);
+
+        addrs[0] = address(0x1234);
+        addrs[1] = address(0x6789);
+        addrs[2] = address(0x8888);
+        addrs[3] = address(0xF1F0);
+
+        amnts[0] = 2e18;
+        amnts[1] = 8e18;
+        amnts[2] = 0.617e18;
+        amnts[3] = 1.38e18;
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = amnts[3];
+        (bytes32 root1, bytes32[] memory proof1) = _computeSimpleTree(addrs, amnts, 3);
+
+        // change the amount at the index chosen for the proof
+        amnts[3] = 7.1e18;
+        values[1] = amnts[3];
+        (bytes32 root2, bytes32[] memory proof2) = _computeSimpleTree(addrs, amnts, 3);
+
+        uint256[] memory rootIds = new uint256[](2);
+        rootIds[0] = rd.addRoot(root1);
+        rootIds[1] = rd.addRoot(root2);
+
+        bytes32[][] memory proofs = new bytes32[][](2);
+        proofs[0] = proof1;
+        proofs[1] = proof2;
+
+        cypher.transfer(address(rd), values[0] + values[1]);
+
+        vm.prank(addrs[3]);
+        rd.claimMultiple(proofs, rootIds, values);
+
+        assertEq(cypher.balanceOf(addrs[3]), values[0] + values[1]);
+        assertTrue(rd.claimed(rootIds[0], addrs[3]));
+        assertTrue(rd.claimed(rootIds[1], addrs[3]));
+    }
+
+    // This test documents the fact that roots may be duplicated in the contract with no issues.
+    function testClaimMultipleDuplicatedRoot() public {
+        (bytes32 root, bytes32[] memory proof, address addr, uint256 amnt) = _genValidRootAndClaimData();
+
+        uint256[] memory rootIds = new uint256[](2);
+        rootIds[0] = rd.addRoot(root);
+        rootIds[1] = rd.addRoot(root);
+
+        bytes32[][] memory proofs = new bytes32[][](2);
+        proofs[0] = proof;
+        proofs[1] = proof;
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = amnt;
+        values[1] = amnt;
+
+        cypher.transfer(address(rd), 2 * amnt);
+
+        vm.prank(addr);
+        rd.claimMultiple(proofs, rootIds, values);
+
+        assertEq(cypher.balanceOf(addr), 2 * amnt);
+        assertTrue(rd.claimed(rootIds[0], addr));
+        assertTrue(rd.claimed(rootIds[1], addr));
+    }
+
+    function testClaimMultipleArrayLengthMismatches() public {
+        (bytes32 root, bytes32[] memory proof, address addr, uint256 amnt) = _genValidRootAndClaimData();
+
+        uint256[] memory rootIds = new uint256[](2);
+        rootIds[0] = rd.addRoot(root);
+        rootIds[1] = rd.addRoot(root);
+
+        bytes32[][] memory proofs = new bytes32[][](2);
+        proofs[0] = proof;
+        proofs[1] = proof;
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = amnt;
+        values[1] = amnt;
+
+        cypher.transfer(address(rd), 2 * amnt);
+
+        uint256[] memory tooManyRootIds = new uint256[](3);
+        tooManyRootIds[0] = rootIds[0];
+        tooManyRootIds[1] = rootIds[1];
+        vm.prank(addr);
+        vm.expectRevert(IRewardDistributor.LengthMismatch.selector);
+        rd.claimMultiple(proofs, tooManyRootIds, values);
+
+        bytes32[][] memory tooFewProofs = new bytes32[][](1);
+        tooFewProofs[0] = proof;
+        vm.prank(addr);
+        vm.expectRevert(IRewardDistributor.LengthMismatch.selector);
+        rd.claimMultiple(tooFewProofs, rootIds, values);
+
+        uint256[] memory tooManyValues = new uint256[](4);
+        tooManyValues[0] = values[0];
+        tooManyValues[1] = values[1];
+        tooManyValues[2] = 1e18;
+        tooManyValues[3] = 0;
+        vm.prank(addr);
+        vm.expectRevert(IRewardDistributor.LengthMismatch.selector);
+        rd.claimMultiple(proofs, rootIds, tooManyValues);
+    }
+
     function _genValidRootAndClaimData()
         internal
         pure
