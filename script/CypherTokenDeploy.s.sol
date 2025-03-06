e2d41e9b0afe8e6c2f98b8f0faf96e30d977c8a1
Elliot <elliotfriedman3@gmail.com>
2025-03-05 20:16:58 -0800

    add Cypher Token Deploy script
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

---
+++ script/CypherTokenDeploy.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.28;
+
+import "forge-std/Script.sol";
+import "src/CypherToken.sol";
+
+contract CypherTokenDeploy is Script {
+    function run() public returns (CypherToken) {
+        // Retrieve treasury address from environment variable
+        address treasury = vm.envAddress("TREASURY_ADDRESS");
+
+        // Start broadcast to record and send transactions
+        vm.startBroadcast();
+
+        // Deploy the CypherToken contract
+        CypherToken token = new CypherToken(treasury);
+
+        // End broadcast
+        vm.stopBroadcast();
+
+        require(token.balanceOf(treasury) == 1_000_000_000 * 1e18, "CypherTokenDeploy: treasury balance mismatch");
+
+        // Return the deployed contract
+        return token;
+    }
+}
