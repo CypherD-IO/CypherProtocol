bfa8c5c4066c173f11369cc3ba09856b20dd4be8
Elliot <elliotfriedman3@gmail.com>
2025-04-04 16:03:51 -0700

    system deploy integration test
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- test/integration/SystemDeploy.t.sol
+++ test/integration/SystemDeploy.t.sol
@@ -5,19 +5,19 @@ import {Addresses} from "lib/forge-proposal-simulator/addresses/Addresses.sol";
 
 import "forge-std/Test.sol";
 
-import {ModuleAdd} from "script/ModuleAdd.s.sol";
+import {SystemDeploy} from "script/SystemDeploy.s.sol";
 import {CypherToken} from "src/CypherToken.sol";
 import {CypherTokenDeploy} from "script/CypherTokenDeploy.s.sol";
 
 import {IntegrationBase} from "test/integration/IntegrationBase.sol";
 
-/// @title ModuleAddIntegration
-/// @notice Integration test for ModuleAdd script against a live system
-/// @dev Run with: forge test --match-path test/integration/ModuleAdd.t.sol -vvv --fork-url $RPC_URL
-contract ModuleAddIntegrationTest is IntegrationBase {
+/// @title SystemDeployIntegration
+/// @notice Integration test for SystemDeploy script against a live system
+/// @dev Run with: forge test --match-path test/integration/SystemDeploy.t.sol -vvv --fork-url $RPC_URL
+contract SystemDeployIntegrationTest is IntegrationBase {
     function testValidateSuccess() public view {
         // Call validate function
-        moduleAdd.validate();
+        systemDeploy.validate();
 
         // Check if the distribution module is enabled
         assertTrue(addresses.isAddressSet("DISTRIBUTION_MODULE"), "Distribution module not set");
