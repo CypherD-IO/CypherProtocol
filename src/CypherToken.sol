03b82dc676a1a4eefb31932241e7a702d4928233
Kurt Barry <kurt.m.barry@gmail.com>
2025-04-29 04:32:18 -0400

    remove ERC20Votes from token

--- src/CypherToken.sol
+++ src/CypherToken.sol
@@ -4,34 +4,13 @@ pragma solidity =0.8.28;
 import {ICypherToken} from "./interfaces/ICypherToken.sol";
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit, Nonces} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import {ERC20Votes, Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import {Time} from "@openzeppelin/contracts/utils/types/Time.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /// @title CypherToken
 /// @notice The token for the Cypher ecosystem.
-contract CypherToken is ICypherToken, ERC20Permit, ERC20Votes {
+contract CypherToken is ICypherToken, ERC20Permit {
     constructor(address treasury) ERC20("Cypher", "CYPR") ERC20Permit("Cypher") {
-        // mint tokens to recipients and handle the rest via transfers from the treasury
+        // mint tokens to a single recipient and handle the rest via transfers from the treasury
         _mint(treasury, 1_000_000_000 * 1e18);
-    }
-
-    function _update(address from, address to, uint256 value) internal override(ERC20Votes, ERC20) {
-        ERC20Votes._update(from, to, value);
-    }
-
-    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
-        return ERC20Permit.nonces(owner);
-    }
-
-    function clock() public view override returns (uint48) {
-        return Time.timestamp();
-    }
-
-    function CLOCK_MODE() public view override returns (string memory) {
-        if (clock() != Time.timestamp()) {
-            revert ERC6372InconsistentClock();
-        }
-        return "mode=timestamp";
     }
 }
--- test/unit/CypherToken.t.sol
+++ test/unit/CypherToken.t.sol
@@ -18,39 +18,4 @@ contract CypherTokenUnitTest is Test {
         assertEq(token.totalSupply(), 1_000_000_000 * 1e18);
         assertEq(token.balanceOf(address(this)), token.totalSupply());
     }
-
-    function testClock(uint256 ts) public {
-        vm.warp(ts % type(uint48).max);
-        assertEq(token.clock(), block.timestamp);
-        assertEq(token.CLOCK_MODE(), "mode=timestamp");
-    }
-
-    function testCheckpointing() public {
-        address other = address(0x1234);
-        address delegate = address(0xde1e947e);
-        assert(other != address(this));
-
-        token.delegate(address(this));
-        assertEq(token.getVotes(address(this)), token.balanceOf(address(this)));
-
-        vm.warp(block.timestamp + 22);
-
-        token.delegate(delegate);
-        assertEq(token.getVotes(address(this)), 0);
-        assertEq(token.getVotes(delegate), token.balanceOf(address(this)));
-
-        vm.warp(block.timestamp + 41);
-
-        token.transfer(other, token.balanceOf(address(this)));
-        assertEq(token.getVotes(delegate), 0);
-        assertEq(token.getVotes(address(this)), 0);
-        assertEq(token.getVotes(other), 0);
-        assertEq(token.numCheckpoints(address(this)), 2);
-        assertEq(token.numCheckpoints(delegate), 2);
-
-        vm.prank(other);
-        token.delegate(other);
-        assertEq(token.getVotes(other), token.balanceOf(address(other)));
-        assertEq(token.numCheckpoints(other), 1);
-    }
 }
